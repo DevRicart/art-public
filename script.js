9934e9f6504e5f5a4cf7964f3de904417ecc5b22
ricardo.ferreira <ricardo.ferreira@fgv.br>
2025-05-19 09:10:18 -0300

    Começando a responsividade

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',     // Movimentação horizontal entre os slides
-      loop: true,                  // Permitir looping infinito
-      slidesPerView: 1,            // Apenas um slide por vez
-      spaceBetween: 0,             // Sem espaço entre os slides
+    direction: 'horizontal',
+      loop: true, 
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+          1440: {
+            slidesPerView: 4,
+          }
+        },
+      spaceBetween: 0,
       pagination: {
         el: '.swiper-pagination',
-        clickable: true,           // Habilitar paginação clicável
+        clickable: true,
       },
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-    });
+});
   
